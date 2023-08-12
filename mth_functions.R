9a418f0198e8c40c2ebac6ce0170d724a6ef7d56
Mirjam Scheib <scheimir@students.zhaw.ch>
2023-08-12 13:35:02 +0200

    commit PC

--- mth_functions.R
+++ mth_functions.R
@@ -34,8 +34,6 @@ basement <- function(model_json, model_new, mesh_path, simulation_json, simulati
   write(model_exp, model_new)
   
   
-  
-  
   #read in simulation json
   simulation <- fromJSON(simulation_json)
   
@@ -45,27 +43,39 @@ basement <- function(model_json, model_new, mesh_path, simulation_json, simulati
   #export 
   simulation_exp <- toJSON(simulation, pretty = TRUE, auto_unbox = TRUE)
   write(simulation_exp, simulation_new)
+ 
+  
+  #setup --------
+    setup_cmd_name <- "c:\\Program Files\\BASEMENT 3.2.0\\bin\\BMv3_BASEplane_setup.exe"
+    
+    setup_param1 = paste("-f ", getwd(), "\\", model_json, sep="")
+    setup_param2 = paste("-o ", getwd(), "\\", setup_h5, sep="")
+    
+    system2(setup_cmd_name, args = c(setup_param1, setup_param2))
   
   
-  # Set up BASEMENT
-  setup_cmd_name <- "c:\\Programme\\BASEMENT 3.2.0\\bin\\BMv3_BASEplane_setup.exe"
-  setup_param1 = paste("-f ", getwd(), "\\", model_json, sep="")
-  setup_param2 = paste("-o ", getwd(), "\\", setup_h5, sep="")
-  system2(setup_cmd_name, args = c(setup_param1, setup_param2))
+  #simulation ---------
+    simulation_cmd_name <- "c:\\Program Files\\BASEMENT 3.2.0\\bin\\BMv3_BASEplane_cudaC.exe"
+    
+    simulation_param1 = paste("-f ", getwd(), "\\", simulation_new, sep="")
+    simulation_param2 = paste("-r ", getwd(), "\\", setup_h5, sep="")
+    simulation_param3 = paste("-o ", getwd(), "\\", results_h5,  sep="")
+    
+    
+    system2(simulation_cmd_name, args = c(simulation_param1, simulation_param2,
+                                          simulation_param3, "-p"))
+
   
-  # Simulation in BASEMENT
-  simulation_cmd_name <- "c:\\Programme\\BASEMENT 3.2.0\\bin\\BMv3_BASEplane_cudaC.exe"
-  simulation_param1 = paste("-f ", getwd(), "\\", simulation_json, sep="") # hier evt. simulation new
-  simulation_param2 = paste("-r ", getwd(), "\\", setup_h5, sep="")
-  simulation_param3 = paste("-o ", getwd(), "\\", results_h5,  sep="")
-  system2(simulation_cmd_name, args = c(simulation_param1, simulation_param2, simulation_param3, "-p"))
-  
-  # Results of BASEMENT
-  results_cmd_name <- "c:\\Programme\\BASEMENT 3.2.0\\bin\\BMv3_BASEplane_results.exe"
-  results_param1 = paste("-f ", getwd(), "\\", results_json, sep="")
-  results_param2 = paste("-r ", getwd(), "\\", results_h5, sep="")
-  results_param3 = paste("-o ", getwd(), "\\", results_xdmf, sep="")
-  system2(results_cmd_name, args = c(results_param1, results_param2, results_param3))
+  #results ---------
+    results_cmd_name <- "c:\\Program Files\\BASEMENT 3.2.0\\bin\\BMv3_BASEplane_results.exe"
+    
+    results_param1 = paste("-f ", getwd(), "\\", results_json, sep="")
+    results_param2 = paste("-r ", getwd(), "\\", results_h5, sep="")
+    results_param3 = paste("-o ", getwd(), "\\", results_xdmf, sep="")
+    
+    system2(results_cmd_name, args = c(results_param1, results_param2, results_param3))
+    
+
 }
 
 ####  HYDRODYNAMIC MODEL RESULTS ####
